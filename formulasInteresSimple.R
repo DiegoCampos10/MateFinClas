5af05af4739ec5dfd83da83a4c3a2c4f65aa487d
DiegoCampos10 <2350470k@umich.mx>
2024-09-19 12:41:11 -0600

    Carga tarea
    Esta carga se sube la primera version de la tarea del interes simple

---
+++ formulasInteresSimple.R
@@ -0,0 +1,12 @@
+#Funciones para cálculo empleando interés simple 
+#Diego E Campos M -- 19/9/24
+
+##Función del valor futuro 
+ VF=function(VA,r,t){
+   xSalida=VA*(1+(r*t))
+   
+   return(xSalida)
+   }
+  
+ 
+   
